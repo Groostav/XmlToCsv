2904b5b5eb4ddd897edd9fd32788f0da3bbcfd60
Geoff - Laptop <geoff_groos@msn.com>
2014-02-01 14:17:07 -0800

    requested change to make code-base testable
    - added public converter class with single method.
    - would suggest pushing the 3 front-ends to use this method instead of
    using their own copy of that identical code.
    - would suggest retro-fitting the remaining library classes with
    "internal" permissions,

---
+++ XmlToCsvConverter/XmlConversionLibrary/XmlToCsvConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using Moor.XmlConversionLibrary.XmlToCsvStrategy;
+
+namespace Moor.XmlConversionLibrary
+{
+    public static class XmlToCsvConverter
+    {
+        public static void ConvertTables(String xmlInputFilePath,
+                                         String outputDirectory)
+        {
+            var converter = new XmlToCsvUsingDataSet(xmlInputFilePath);
+            var context = new XmlToCsvContext(converter);
+
+            foreach (string xmlTableName in context.Strategy.TableNameCollection)
+            {
+                context.Execute(xmlTableName, outputDirectory + @"\" + xmlTableName + ".csv", Encoding.Unicode);
+            }
+        } 
+
+        //could add a method that takes a factory:
+        //public static void ConvertTables(String xmlInputFilePath, String outputDirectory, 
+        //                                 Func<String, XmlToCsvStrategyBase> strategyFactory);
+        //this would allow your consumers to pick the strategy they wish to use,
+        //which adds complexity but also adds features (?)
+    }
+}
